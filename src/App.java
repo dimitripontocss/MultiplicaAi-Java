58b60af2192738e5871564c9963d27afe574e0fe
Dimitri <assis.dimitri@engenharia.ufjf.br>
2023-01-09 18:44:56 -0300

    Primeira Melhora

--- src/App.java
+++ src/App.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in); // Create a Scanner object
-        System.out.println("Escolha o número da tabuada:");
+        try (Scanner myObj = new Scanner(System.in)) {
+            System.out.println("Escolha o número da tabuada:");
 
-        int number = Integer.parseInt(myObj.nextLine());
-        System.out.println("Tabela de multiplicação de " + number);
+            int number = Integer.parseInt(myObj.nextLine());
+            System.out.println("Tabela de multiplicação de " + number);
 
-        for (int cont = 1; cont < 11; cont++) {
-            System.out.println(number + "x" + cont + " = " + cont * number);
+            for (int cont = 1; cont < 11; cont++) {
+                System.out.println(number + "x" + cont + " = " + cont * number);
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
     }
 }
